72b417faa9bd2073cc8e2f065b8d048d782df543
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-09-07 17:55:15 -0300

    Finalizar ex1

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-let livro = {
-    au
+const modulosDoCurso = ['finaças', 'marketing', 'soft-skills', 'vendas']
+
+let listaDePessoasEstudantes =[
+    {
+        nome : 'Júnior',
+        idade: 35,
+        cpf: 96780541087, 
+        modulo: modulosDoCurso[0]
+    },
+]
+
+const pessoaEstudante = {
+    nome : 'Fayra',
+    idade: 29,
+    cpf: 91349341037, 
+    modulo: modulosDoCurso[1]
 }
+
+
+
+console.log(listaDePessoasEstudantes)
