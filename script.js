5f5ac0f1a7807ed4de93593f7bd9b233f185e2ee
Nicole Tsingas <nicolecass27@gmail.com>
2023-09-18 14:36:26 -0300

    Realizado exercicio da entrevista tech

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ const pessoaEstudante = {
     cpf: 91349341037, 
     modulo: modulosDoCurso[1]
 }
+
+listaDePessoasEstudantes.push(pessoaEstudante);
+
+//console.log(listaDePessoasEstudantes);
+
+let copia = pessoaEstudante;
+
+console.log(copia);
+
+copia = {
+    nome: 'Nicole',
+    idade: 24,
+    cpf: 46842614848,
+    modulo: modulosDoCurso[3],
+}
+
+//console.log(copia);
+//console.log(pessoaEstudante);
+
+listaDePessoasEstudantes.push(copia);
+//console.log(listaDePessoasEstudantes);
+
+const novoArray = listaDePessoasEstudantes.filter((pessoa) => pessoa.idade > 20);
+
+console.log(novoArray);
+
+const novaIdade = listaDePessoasEstudantes.map((estudante) => estudante.idade + 1);
+
+console.log(novaIdade);
